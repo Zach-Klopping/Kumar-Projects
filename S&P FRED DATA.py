c74577f5730763fdf029a3eca7fe0cca10b3eb31
Zach-Klopping <zach.klopping@gmail.com>
2024-11-03 12:57:31 -0600

    Kumar Files

---
+++ S&P FRED DATA.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+pd.set_option("display.max_columns", 1000)
+
+from fredapi import Fred
+fred_key = "981efcd7e9faf439aa70c00d5ae16089"
+
+fred=Fred(api_key=fred_key)
+
+
+sp_search= fred.search("S&P", order_by="popularity")
+
+sp_search2=fred.get_series("SP500")
+#print(sp_search2)
+
+plt.plot(sp_search2)
+plt.title("S&P500")
+plt.show()
